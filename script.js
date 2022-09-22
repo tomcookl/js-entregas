c965b793f679706bf4b40ba7d016f84673e3bd5a
tomcookl <tomascooklastra2.0@gmail.com>
2022-09-21 21:35:33 -0300

    eventos

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-let repuesta = true
-do{
-    let pregunta = prompt("Estas listo para comenzar? Rta:si o no")
-    if(pregunta.toLowerCase()=="si"){
-        console.log("El usuario esta listo para comenzar")
-        repuesta=false
+const notas = []
+let idUnico = 0
+
+class Nota {
+    construcutor(idea) {
+        this.idea = idea
+        this.id = idUnico++
     }
-}while(repuesta)
-let condicion = true
-do {
-    let pregunta = prompt("En que equipo debuto Maradona?")
-    if(pregunta.toLowerCase()== "argentinos juniors"){
-        console.log ("Su repuesta es correcta")
-        condicion=false
-    }else{
-        console.log ("Su respuesta no es correcta")
-    }
-} while(condicion)
-let napoli = true
-do{
-    let pregunta = prompt("En que equipo italiano ocupo un rol clave Maradona?")
-    if (pregunta.toLowerCase() == "napoli"){
-        console.log ("Su respuesta es correcta")
-        napoli = false
-    }else{
-        console.log("Su respuesta no es correcta")
-    }
-}while(napoli)
-let mundial = true
-do{
-    let pregunta = prompt("En que año fue campeon del mundo?")
-    if(pregunta== 1986){
-        console.log("Su respuesta es correcta")
-        mundial=false
-    }else{
-        console.log("Su respuesta no es correcta")
-    }
-}while(mundial)
+}
+
+const inputNota = document.getElementById('inputNota')
+const btnGuardar = document.getElementById('btnGuardar')
+const borrarTodo = document.getElementById('borrarTodo')
+const listaNotas = document.getElementById('listaNotas')
+
+btnGuardar.addEventListener('click', () => {
+    notas.push(new Nota(inputNota.value))
+    console.log(notas)
+})
